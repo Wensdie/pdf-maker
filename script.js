d3ca8151b375b57a691ff511376a098b387ab48d
Maciej Malinowski <maciekmalinowski12@gmail.com>
2024-04-26 21:19:02 +0200

    feat: Add Puppeteer and Dotenv

---
+++ script.js
@@ -0,0 +1,25 @@
+import puppeteer from "puppeteer";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const browser = await puppeteer.launch({
+  headless: 'new',
+  deafultViewport: null,
+});
+
+const page = await browser.newPage();
+await page.goto('https://www.google.pl', {
+  waitUntil: 'domcontentloaded' 
+});
+
+// const emailInput = await page.waitForSelector("#loginform-username");
+// const passInput = await page.waitForSelector("#loginform-password");
+
+// await page.focus(emailInput);
+// page.keyboard.type(process.env.PDF_EMAIL, {delay: 100});
+
+// await page.focus(passInput);
+// page.keyboard.type(process.env.PDF_PASSWORD, {delay: 100});  
+
+// await browser.close();
